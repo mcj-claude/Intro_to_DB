9208de4a9e46fedcc4a078b89be2c1cce9c27e79
Mcjclaude-2025 <trustedgadgets2002@gmail.com>
2025-12-14 23:05:39 +0300

    message

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Print full description of books table from alx_book_store database
 -- Database name will be passed as argument of mysql command
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-AND TABLE_NAME = 'books'
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books'
 ORDER BY ORDINAL_POSITION;
