27305e50132af56b57b0b32ff74662f9edca5348
Mcjclaude-2025 <trustedgadgets2002@gmail.com>
2025-12-14 23:03:15 +0300

    message

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,5 @@
 -- Print full description of books table from alx_book_store database
 -- Database name will be passed as argument of mysql command
--- Cannot use DESCRIBE or EXPLAIN statements
 
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
